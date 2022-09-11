a111d3d131c8c83eb2cf68f838b21807895af42f
Noah Carralero <nole2kgames@gmail.com>
2022-09-11 19:37:21 -0400

    edited routes and package files

--- routes/notes.js
+++ routes/notes.js
@@ -2,29 +2,42 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 
-app.get('/api/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../db/db.json'));
-});
 
-app.post('/api/notes', (req, res) => {
-  let db = fs.readFileSync('db/db.json');
-  db = JSON.parse(db);
-  res.json(db);
-  let userNote = {
-    title: req.body.title,
-    text: req.body.text,
-  };
-  db.push(userNote);
-  fs.writeFileSync('db/db.json', JSON.stringify(db));
-  res.json(db);
-});
-
-app.delete('/api/notes/:id', (req, res) => {
-  let db = JSON.parse(fs.readFileSync('db/db.json'))
-  let deleteNotes = db.filter(item => item.id !== req.params.id);
-  fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
-  res.json(deleteNotes);
-});
-
+router.get("/", (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        throw err;
+      } else {
+        const notes = JSON.parse(data);
+        res.json(notes);
+      }
+    });
+  });
+  
+  router.post("/", (req, res) => {
+    console.log(req.body);
+    const newNote = {
+      title: req.body.title,
+      text: req.body.text,
+    };
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        throw err;
+      } else {
+        const notes = JSON.parse(data);
+        notes.push(newNote);
+        fs.writeFile(
+          "./db/db.json",
+          JSON.stringify(notes, null, 4),
+          (err, data) => {
+            if (err) {
+              throw err;
+            }
+            res.json({ data: req.body, message: "success!" });
+          }
+        );
+      }
+    });
+  });
   
 module.exports = router;
